94a5df5a048b4dc9ace1b6529c788beb77589a6f
Addy56 <60960831+Addy56@users.noreply.github.com>
2022-02-23 22:55:15 +0530

    Update Slip28b.py

--- PYTHON/Slip28b.py
+++ PYTHON/Slip28b.py
@@ -1,14 +1,15 @@
 # Write a Python program to accept two lists and merge the two lists into list of tuple
 list_a = []
 list_b = []
-print("number of elements to enter in both list")
+print("number of elements to enter in first list")
 no = int(input())
 print("enter first list of elements")
 for i in range(0,no):
     ele = int(input())
     list_a.append(ele) 
-print("enter second list of elements")
+print("number of elements to enter in second list")
 no = int(input())
+print("enter second list of elements")
 for i in range(0,no):
     ele = int(input())
     list_b.append(ele) 
